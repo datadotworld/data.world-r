67f61904e3bd3ecd80bd2812fffe9e40111f9dd3
bryon <bryon@data.world>
2017-01-19 08:00:48 -0600

    refactored

--- R/data.world.R
+++ R/data.world.R
@@ -1,56 +1,10 @@
-# data.world client library
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
-library(httr)
-library(readr)
-
 #' data.world client constructor
 #' @param token your data.world API token
 #'
 #' @return a data.world client
 #' @export
 data.world <- function(token) {
-  thisEnv <- environment()
-  token = token
-  me <- list(
-    thisEnv = thisEnv,
-    getToken =
-      #' @rdname getToken
-      #' @export
-      function() {
-        return(get("token", thisEnv))
-      },
-    query =
-      #' @rdname query
-      #' @export
-      #'
-      #' @param type     the type of the query - either "sql" or "sparql"
-      #' @param dataset  the "agentid/datasetid" for the dataset against which to execute the query
-      #' @param query    the SQL or SPARQL query to run
-      #'
-      #' @return the query results as a data frame
-      function(type="sql", dataset, query) {
-        "execute a SQL or SPARQL query against a data.world dataset"
-        url = sprintf("https://query.data.world/%s/%s", type, dataset)
-        response <- GET( url,
-                         query = list(query = query),
-                         add_headers(
-                           Accept = "text/csv",
-                           Authorization = sprintf("Bearer %s", get("token", thisEnv))
-                         ))
-        content(response)
-      }
-  )
-  assign('this',me,envir=thisEnv)
-  class(me) <- append(class(me), "data.world")
+  me <- list( token = token )
+  class(me) <- "data.world"
   return(me)
 }
---
+++ R/query.R
@@ -0,0 +1,29 @@
+#' execute a SQL or SPARQL query against a data.world client
+#'
+#' @param connection the connection to data.world
+#' @param type       the type of the query - either "sql" or "sparql"
+#' @param dataset    the "agentid/datasetid" for the dataset against which to execute the query
+#' @param query      the SQL or SPARQL query to run
+#'
+#' @return the query results as a data frame
+#' @export
+query <- function(connection, type, dataset, query) {
+  UseMethod("query")
+}
+
+#' @export
+query.default <- function(connection, type, dataset, query) {
+  print("nope.")
+}
+
+#' @export
+query.data.world <- function(connection, type = "sql", dataset, query) {
+  url = sprintf("https://query.data.world/%s/%s", type, dataset)
+  response <- httr::GET( url,
+                   query = list(query = query),
+                   httr::add_headers(
+                     Accept = "text/csv",
+                     Authorization = sprintf("Bearer %s", connection$token)
+                   ))
+  httr::content(response)
+}

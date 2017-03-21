a1eafb6a4a2c16e80e4b1bf8c9034d3a2ce3d8fa
Triet Le <trietminh@gmail.com>
2017-03-21 11:34:36 -0500

    clean up

--- demo/util.R
+++ demo/util.R
@@ -1,12 +1,20 @@
 
 set_default_demo_variables <- function() {
   timestamp <- strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y%m%dT%H%M%S")
-  demo_dataset_owner <- readline("data.world user_id the current token belong to: ")
+  if (exists("default_dataworld_user_id")) {
+    demo_dataset_owner <- readline(sprintf("data.world user_id the current token belong to  (default to %s): ", default_dataworld_user_id))
+    demo_dataset_owner = if (demo_dataset_owner == '') default_dataworld_user_id else demo_dataset_owner
+  } else {
+    demo_dataset_owner <- readline("data.world user_id the current token belong to: ")
+  }
+
   default_dataset_title <- sprintf('demoDataset%s', timestamp)
   demo_dataset_title <- readline(sprintf("dataset title for this demo (default to %s) : ", default_dataset_title))
   demo_dataset_title = if (demo_dataset_title == '') default_dataset_title else demo_dataset_title
   message(sprintf("default dataset owner for this demo is set to %s", demo_dataset_owner))
   message(sprintf("default dataset title for this demo is set to %s", demo_dataset_title))
+  ret <- list("dataset_owner" = demo_dataset_owner, "dataset_title" = demo_dataset_title)
+  return(ret)
 }
 
 
@@ -18,4 +26,9 @@ verify_token <- function() {
     message("current token appears to be invalid.")
     message("Please make sure to follow instruction on how to retrieve a data.world token.\nSee \'vignette(\"quickstart\", package = \"data.world\")\' for further instruction.")
   }
+}
+
+dataset_url <- function(dataset_owner, dataset_title) {
+  dataset_url <- sprintf("https://data.world/%s/%s", dataset_owner, dataset_title)
+  return(dataset_url)
 }

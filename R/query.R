eaa22c4da6cf304c3cd5aff955cea7e49a7b450f
tle <trietminh@gmail.com>
2017-03-26 09:00:34 -0500

    add parameters to query.R (#5)

--- R/query.R
+++ R/query.R
@@ -19,7 +19,8 @@ This product includes software developed at data.world, Inc.(http://www.data.wor
 #' @param connection the connection to data.world
 #' @param type       the type of the query - either "sql" or "sparql"
 #' @param dataset    the "agentid/datasetid" for the dataset against which to execute the query
-#' @param query      the SQL or SPARQL query to run
+#' @param query      the SQL or SPARQL query to run .
+#' @param queryParameters Optional comma-separated ?name=value pairs
 #' @param ...  additional param
 #' @return the query results as a data frame
 #' @seealso \code{\link{data.world}}
@@ -29,31 +30,59 @@ This product includes software developed at data.world, Inc.(http://www.data.wor
 #'       query="SELECT *
 #'                FROM TableName
 #'               LIMIT 10")
+#'
+#' query(connection, dataset="user/dataset",
+#'       query="SELECT *
+#'                FROM TableName where `field1` = ? AND `field2` > ?
+#'               LIMIT 10",
+#'       queryParameters = list("value", 5.0))
+#'
 #' query(connection, dataset="user/dataset", type="sparql",
 #'       query="SELECT *
 #'              WHERE {
 #'                ?s ?p ?o.
 #'              } LIMIT 10")
+#'
+#' query(connection, dataset="user/dataset", type="sparql",
+#'       query="SELECT *
+#'              WHERE {
+#'              [ :Year ?year ; :Region ?region ; :Indicator_Coverage_and_Disaggregation ?score ]
+#'              FILTER(?score > $v1)
+#'              } LIMIT 10",
+#'              queryParameters = list("$v1"=5.5))
+#'
 #' @export
-query <- function(connection, type, dataset, query, ...) {
+query <- function(connection, type, dataset, query, queryParameters = list(), ...) {
   UseMethod("query")
 }
 
 #' @export
-query.default <- function(connection, type, dataset, query, ...) {
+query.default <- function(connection, type, dataset, query, queryParameters = list(), ...) {
   print("nope.")
 }
 
 #' @export
-query.data.world <- function(connection, type = "sql", dataset, query, ...) {
+query.data.world <- function(connection, type = "sql", dataset, query, queryParameters = list(), ...) {
   url = sprintf("https://query.data.world/%s/%s", type, dataset)
-
+  requestQuery = list(query = query)
+  if (length(queryParameters) > 0) {
+    if (type == "sparql") {
+      requestQuery$parameters <- contructQueryParameterString(queryParameters)
+    } else {
+      namedQueryParmeters <- list()
+       for (i in 0:(length(queryParameters)-1)) {
+          namedQueryParmeters[[paste("$data_world_param",i, sep = "")]] <- queryParameters[[i+1]]
+       }
+      requestQuery$parameters <- contructQueryParameterString(namedQueryParmeters)
+    }
+  }
   response <- httr::GET( url,
-                   query = list(query = query),
+                   query = requestQuery,
                    httr::add_headers(
                      Accept = "text/csv",
                      Authorization = sprintf("Bearer %s", connection$token)),
                    httr::user_agent(data.world::userAgent()))
+  #print(response)
   ret <- httr::http_status(response)
   if (response$status_code == 200) {
     text <- httr::content(x=response, as='text')
@@ -61,4 +90,27 @@ query.data.world <- function(connection, type = "sql", dataset, query, ...) {
     ret <- df
   }
   ret
+}
+
+contructQueryParameterString <- function (queryParameterNamedList) {
+  queryParameterStrings = lapply(names(queryParameterNamedList), function (param) {
+    sparqlLiteral <- convertToSparqlLiteral(queryParameterNamedList[[param]])
+    paste(param, sparqlLiteral, sep = "=")
+  })
+  ret <- paste(queryParameterStrings, collapse = ",")
+  ret
+}
+
+convertToSparqlLiteral <- function (v) {
+  type <- class(v)
+  iriTemplate <- switch(type,
+         "logical"="\"%s\"^^<http://www.w3.org/2001/XMLSchema#boolean>",
+         "numeric"="\"%s\"^^<http://www.w3.org/2001/XMLSchema#decimal>",
+         "integer"="\"%s\"^^<http://www.w3.org/2001/XMLSchema#integer>",
+         "character"="\"%s\""
+         )
+  if(is.null(iriTemplate)) {
+    stop(sprintf("parameter value %s is of not supported type %s. Supported types are logical, numeric, integer, character.", v, type))
+  }
+  return(sprintf(iriTemplate, v))
 }
--- tests/testthat/testQuery.R
+++ tests/testthat/testQuery.R
@@ -18,11 +18,12 @@ library("data.world")
 library("testthat")
 source("testUtil.R")
 
-test_that("query making the correct HTTR request" , {
+test_that("sparql query making the correct HTTR request" , {
   sqlQuery = "SELECT * FROM TableName LIMIT 10"
-  type = "sql"
+  type = "sparql"
   dataset = "ownerid/datasetid"
   mockResponseLocalContentPath = "resources/file1.csv"
+  queryParameters = list(key1="value1", "?key2"=1L, "?key3"=1, "?key4"=TRUE, "?key5"=1.5)
   response <- with_mock(
     `httr::GET` = function(url, query, header , userAgent)  {
       expect_equal(url,
@@ -30,12 +31,40 @@ test_that("query making the correct HTTR request" , {
       expect_equal(header$headers[["Authorization"]], "Bearer API_TOKEN")
       expect_equal(header$headers[["Accept"]], "text/csv")
       expect_equal(query[["query"]], sqlQuery)
+      expect_equal(query[["parameters"]], "key1=\"value1\",?key2=\"1\"^^<http://www.w3.org/2001/XMLSchema#integer>,?key3=\"1\"^^<http://www.w3.org/2001/XMLSchema#decimal>,?key4=\"TRUE\"^^<http://www.w3.org/2001/XMLSchema#boolean>,?key5=\"1.5\"^^<http://www.w3.org/2001/XMLSchema#decimal>")
       expect_equal(userAgent$options$useragent, data.world::userAgent())
       return(successMessageResponseWithContent(mockResponseLocalContentPath, "application/csv"))
     },
     `mime::guess_type` = function(...)
       NULL,
-    data.world::query(data.world(token = "API_TOKEN"), dataset = dataset , query = sqlQuery , type = type)
+    data.world::query(data.world(token = "API_TOKEN"), dataset = dataset , query = sqlQuery , type = type, queryParameters = queryParameters)
+  )
+  expect_equal(is.data.frame(response), TRUE)
+  expected = read.csv(mockResponseLocalContentPath)
+  expect_equal(all(expected == as.data.frame(response)), TRUE)
+})
+
+
+test_that("sql query making the correct HTTR request" , {
+  sqlQuery = "SELECT * FROM TableName LIMIT 10"
+  type = "sql"
+  dataset = "ownerid/datasetid"
+  mockResponseLocalContentPath = "resources/file1.csv"
+  queryParameters = list("value1", 1L, 1, TRUE, 1.5)
+  response <- with_mock(
+    `httr::GET` = function(url, query, header , userAgent)  {
+      expect_equal(url,
+                   sprintf("https://query.data.world/%s/%s", type, dataset))
+      expect_equal(header$headers[["Authorization"]], "Bearer API_TOKEN")
+      expect_equal(header$headers[["Accept"]], "text/csv")
+      expect_equal(query[["query"]], sqlQuery)
+      expect_equal(query[["parameters"]], "$data_world_param0=\"value1\",$data_world_param1=\"1\"^^<http://www.w3.org/2001/XMLSchema#integer>,$data_world_param2=\"1\"^^<http://www.w3.org/2001/XMLSchema#decimal>,$data_world_param3=\"TRUE\"^^<http://www.w3.org/2001/XMLSchema#boolean>,$data_world_param4=\"1.5\"^^<http://www.w3.org/2001/XMLSchema#decimal>")
+      expect_equal(userAgent$options$useragent, data.world::userAgent())
+      return(successMessageResponseWithContent(mockResponseLocalContentPath, "application/csv"))
+    },
+    `mime::guess_type` = function(...)
+      NULL,
+    data.world::query(data.world(token = "API_TOKEN"), dataset = dataset , query = sqlQuery , type = type, queryParameters = queryParameters)
   )
   expect_equal(is.data.frame(response), TRUE)
   expected = read.csv(mockResponseLocalContentPath)
